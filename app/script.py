a263e65b4a404d4f2b88820fd674b3961277d2c3
Marcelo Delazari <marceloalvesdelazari@gmail.com>
2022-01-13 14:17:14 -0300

    Update script.py

--- app/script.py
+++ app/script.py
@@ -1,8 +1,26 @@
 from flask import Flask, render_template
 from urllib.request import Request, urlopen
+import random
+
+def getRandomLink():
+    num = random.randrange(100000, 999999)
+    site = "http://prnt.sc/" + str(num)
+    return site
+
+def validateImage(imgur_link):
+    if "prntscr" in imgur_link:
+        print(imgur_link)
+        return False
+
+    imgur_req = Request(imgur_link, headers={'User-Agent': 'Mozilla/5.0'})
+    imgur_html = str(urlopen(imgur_req).read())
+    print(imgur_html)
+    if 'content="https://www.facebook.com/imgur">  <script src=' in imgur_html:
+        print(imgur_link)
+        return False
+    return True
 
 app = Flask(__name__)
-from utils import getRandomLink, validateImage
 
 @app.route('/')
 def index():
