0fdaf9aa3fe3385c0fc78e60a5918bf49bf70c6b
Marcelo Delazari <marceloalvesdelazari@gmail.com>
2022-01-13 14:42:41 -0300

    Update script.py

--- app/script.py
+++ app/script.py
@@ -25,24 +25,7 @@ app = Flask(__name__, template_folder='templates')
 @app.route('/')
 def index():
 
-    found_image = False
-    while not found_image:
-
-        site = getRandomLink()
-        req = Request(site, headers={'User-Agent': 'Mozilla/5.0'})
-        html = str(urlopen(req).read())
-
-        img_class = "twitter:image:src"
-        img_class_index = html.index(img_class)
-        img_link_ini = img_class_index + len(img_class) + 11
-        img_link_end = img_link_ini + html[img_link_ini:].index('"')
-        img_link = html[img_link_ini:img_link_end]
-
-        imgur_link = img_link[:len(img_link) - 4]
-
-        found_image = validateImage(imgur_link)
-        print(found_image)
-    return render_template('index.html', random_img=img_link)
+    return render_template('index.html', random_img="https://i.imgur.com/r7QL273.png")
 
 
 if __name__ == "__main__":
